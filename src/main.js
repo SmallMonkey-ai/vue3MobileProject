e083042af20dcc9e89a0a1b58dcd6495f5d60c9a
SmallMonkey-ai <2641973190@qq.com>
2022-10-19 13:25:30 +0800

    删除之前vuex 配置

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+
 
 // 移动端适配
 import 'lib-flexible/flexible.js';
@@ -11,4 +11,4 @@ import '@/assets/scss/index.scss';
 // 全局引入按需引入UI库 vant
 import { vantPlugins } from './plugins/vant.js'
 
-createApp(App).use(store).use(router).use(vantPlugins).mount('#app')
+createApp(App).use(router).use(vantPlugins).mount('#app')
